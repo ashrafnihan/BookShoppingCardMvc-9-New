b744a3c7c960d4bfe77b827fa9acf37797092846
Nihanbhai <ashrafnihan74@gmail.com>
2025-08-02 11:27:14 +0600

    Add project files.

---
+++ BookShoppingCardMvcUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookShoppingCardMvcUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
