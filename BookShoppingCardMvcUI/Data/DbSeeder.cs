491548af53e0cdc22293ef5e6d924cc089409a34
Nihanbhai <ashrafnihan74@gmail.com>
2025-09-05 21:47:15 +0600

    Implement DbSeeder to seed default roles and admin user

--- BookShoppingCardMvcUI/Data/DbSeeder.cs
+++ BookShoppingCardMvcUI/Data/DbSeeder.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using BookShoppingCardMvcUI.Constants;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
 
 namespace BookShoppingCardMvcUI.Data
 {
     public class DbSeeder
     {
-        public static void SeedDefaultData(IServiceProvider service)
+        public static async Task SeedDefaultData(IServiceProvider service)
         {
             var userMgr = service.GetService<UserManager<IdentityUser>>();
-            
+            var roleMgr = service.GetService<RoleManager<IdentityRole>>();
+            //adding some rows to db 
+            await roleMgr.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleMgr.CreateAsync(new IdentityRole(Roles.User.ToString()));
+            // create admin user
+            var admin = new IdentityUser
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true
+            };
+
+            var userInDB = userMgr.FindByEmailAsync(admin.Email);
+            if (userInDB is null)
+            {
+                await userMgr.CreateAsync(admin, "Admin@123");
+                await userMgr.AddToRoleAsync(admin, Roles.Admin.ToString());
+            }
 
         }
     }
