0bede5ab5b858f4975e02117ccdb0c26ffb4a544
Nihanbhai <ashrafnihan74@gmail.com>
2025-09-05 21:50:48 +0600

    Enhance Register page logic with role assignment and user registration improvements

--- BookShoppingCardMvcUI/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookShoppingCardMvcUI/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -120,6 +120,7 @@ namespace BookShoppingCardMvcUI.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
+                    await _userManager.AddToRoleAsync(user, Constants.Roles.User.ToString());
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
