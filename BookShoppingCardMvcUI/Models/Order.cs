93a6bdb8172bfa1d1b879103fb4651d7a3f29035
Nihanbhai <ashrafnihan74@gmail.com>
2025-09-05 21:46:54 +0600

    Create Order entity with properties, relationships, and data annotations

--- BookShoppingCardMvcUI/Models/Order.cs
+++ BookShoppingCardMvcUI/Models/Order.cs
@@ -7,7 +7,7 @@ namespace BookShoppingCardMvcUI.Models
     public class Order
     {
         public  int Id { get; set; }
-        public string? UserId { get; set; }
+        public string UserId { get; set; }
         public DateTime CreateDate { get; set; } = DateTime.UtcNow;
         [Required]
         public int OrderStatusId { get; set;  }
